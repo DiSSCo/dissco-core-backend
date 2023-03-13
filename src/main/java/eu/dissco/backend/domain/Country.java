923cb54597528b0420d44426994589aeb808c734
southeo <soulaine.theo@gmail.com>
2023-03-13 10:48:13 +0100

    Remove organisation and mapping data flow

--- src/main/java/eu/dissco/backend/domain/Country.java
+++
@@ -1,3 +0,0 @@
-package eu.dissco.backend.domain;
-
-public record Country(String country, String countryCode) { }
--- src/main/java/eu/dissco/backend/domain/OrganisationTuple.java
+++
@@ -1,5 +0,0 @@
-package eu.dissco.backend.domain;
-
-public record OrganisationTuple(String name, String ror) {
-
-}
