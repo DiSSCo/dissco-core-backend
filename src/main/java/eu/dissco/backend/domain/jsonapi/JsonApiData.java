804700ffb8b75b531b8aa8a576925606d1ab9161
southeo <soulaine.theo@gmail.com>
2023-04-06 11:08:27 +0200

    Fix user endpoint

--- src/main/java/eu/dissco/backend/domain/jsonapi/JsonApiData.java
+++ src/main/java/eu/dissco/backend/domain/jsonapi/JsonApiData.java
@@ -1,24 +1,30 @@
 package eu.dissco.backend.domain.jsonapi;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Value;
 
 @Value
 public class JsonApiData {
+  @JsonProperty("id")
   String id;
+  @JsonProperty("type")
   String type;
+  @JsonProperty("attributes")
   JsonNode attributes;
 
-  public <T extends Record> JsonApiData(String id, String type, T domainObject, ObjectMapper mapper){
-    this.id = id;
-    this.type = type;
-    this.attributes = mapper.valueToTree(domainObject);
-  }
+  @JsonCreator
   public JsonApiData(String id, String type, JsonNode attributes){
     this.id = id;
     this.type = type;
     this.attributes = attributes;
   }
 
+  public <T extends Record> JsonApiData(String id, String type, T domainObject, ObjectMapper mapper){
+    this.id = id;
+    this.type = type;
+    this.attributes = mapper.valueToTree(domainObject);
+  }
 }
