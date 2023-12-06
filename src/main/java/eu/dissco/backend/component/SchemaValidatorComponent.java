41d2bfb9e9df45071be864f1d31c9bec216196bd
southeo <soulaine.theo@gmail.com>
2023-12-06 13:24:43 +0100

    add validation logging

--- src/main/java/eu/dissco/backend/component/SchemaValidatorComponent.java
+++ src/main/java/eu/dissco/backend/component/SchemaValidatorComponent.java
@@ -24,6 +24,7 @@ public class SchemaValidatorComponent {
     if (errors.isEmpty()) {
       return;
     }
+    log.error("Invalid annotation request. Errors {}", errors);
     throw new InvalidAnnotationRequestException(errors.toString());
   }
 
