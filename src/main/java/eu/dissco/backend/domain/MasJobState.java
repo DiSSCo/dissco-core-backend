a82574c5c6d8f427b9d11eddd3259823f414f870
southeo <soulaine.theo@gmail.com>
2023-12-21 14:41:37 +0100

    Add mjr target type

--- src/main/java/eu/dissco/backend/domain/MasJobState.java
+++ src/main/java/eu/dissco/backend/domain/MasJobState.java
@@ -1,15 +1,9 @@
 package eu.dissco.backend.domain;
 
 public enum MasJobState {
-  SCHEDULED("SCHEDULED"),
-  FAILED("FAILED"),
-  COMPLETED("COMPLETED"),
-  RUNNING("RUNNING");
-
-  private final String state;
-
-  MasJobState(String s) {
-    this.state = s;
-  }
+  SCHEDULED,
+  FAILED,
+  COMPLETED,
+  RUNNING;
 
 }
