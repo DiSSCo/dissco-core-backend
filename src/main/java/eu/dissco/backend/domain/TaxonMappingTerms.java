3f60b58ad9523bfb214bf831528b8aa63e435654
Sam Leeflang <sam.leeflang@naturalis.nl>
2024-03-27 09:12:42 +0100

    Sonar review

--- src/main/java/eu/dissco/backend/domain/TaxonMappingTerms.java
+++ src/main/java/eu/dissco/backend/domain/TaxonMappingTerms.java
@@ -1,6 +1,5 @@
 package eu.dissco.backend.domain;
 
-import eu.dissco.backend.exceptions.UnknownParameterException;
 import java.util.HashMap;
 import java.util.Map;
 import lombok.extern.slf4j.Slf4j;
