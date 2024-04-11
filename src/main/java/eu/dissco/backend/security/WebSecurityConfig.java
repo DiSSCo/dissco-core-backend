e25db31d966a122ffc994e2944cc3d018ae8ac51
Sam Leeflang <sam.leeflang@naturalis.nl>
2024-04-11 16:16:16 +0200

    Make health endpoint accessible

--- src/main/java/eu/dissco/backend/security/WebSecurityConfig.java
+++ src/main/java/eu/dissco/backend/security/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package eu.dissco.backend.security;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.actuate.autoconfigure.security.servlet.EndpointRequest;
+import org.springframework.boot.actuate.health.HealthEndpoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -19,6 +21,7 @@ public class WebSecurityConfig {
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(authorizeHttpRequests -> authorizeHttpRequests
+        .requestMatchers(EndpointRequest.to(HealthEndpoint.class)).permitAll()
         .requestMatchers(HttpMethod.GET, "**").permitAll()
         .anyRequest().authenticated());
 
