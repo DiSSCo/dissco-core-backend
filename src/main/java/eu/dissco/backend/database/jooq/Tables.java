6982153a4bc4eb4ec8787dbc26e44ae64d418687
southeo <soulaine.theo@gmail.com>
2024-05-28 13:40:13 +0200

    jooq

--- src/main/java/eu/dissco/backend/database/jooq/Tables.java
+++ src/main/java/eu/dissco/backend/database/jooq/Tables.java
@@ -19,7 +19,7 @@ import eu.dissco.backend.database.jooq.tables.User;
 public class Tables {
 
     /**
-     * The table <code>public.annotations</code>.
+     * The table <code>public.annotation</code>.
      */
     public static final Annotation ANNOTATION = Annotation.ANNOTATION;
 
