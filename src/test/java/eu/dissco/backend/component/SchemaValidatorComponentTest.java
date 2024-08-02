47d354cdfe057cfdc8e25e19c5456362492ec77d
southeo <soulaine.theo@gmail.com>
2024-08-02 13:45:29 +0200

    imports

--- src/test/java/eu/dissco/backend/component/SchemaValidatorComponentTest.java
+++ src/test/java/eu/dissco/backend/component/SchemaValidatorComponentTest.java
@@ -9,7 +9,6 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import eu.dissco.backend.domain.annotation.batch.AnnotationEventRequest;
 import eu.dissco.backend.domain.annotation.batch.BatchMetadata;
 import eu.dissco.backend.exceptions.InvalidAnnotationRequestException;
-import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.stream.Stream;
