d14a5647cd90e259af6fee610dab1945a16ee38e
Sam Leeflang <sam.leeflang@naturalis.nl>
2025-10-28 08:34:39 +0100

    Formatting

--- src/main/java/eu/dissco/backend/configuration/ElasticSearchConfiguration.java
+++ src/main/java/eu/dissco/backend/configuration/ElasticSearchConfiguration.java
@@ -7,6 +7,7 @@ import co.elastic.clients.transport.rest5_client.Rest5ClientTransport;
 import co.elastic.clients.transport.rest5_client.low_level.Rest5Client;
 import co.elastic.clients.transport.rest5_client.low_level.Rest5ClientBuilder;
 import eu.dissco.backend.properties.ElasticSearchProperties;
+import java.util.Base64;
 import lombok.RequiredArgsConstructor;
 import org.apache.hc.core5.http.Header;
 import org.apache.hc.core5.http.HttpHost;
@@ -14,26 +15,24 @@ import org.apache.hc.core5.http.message.BasicHeader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.Base64;
-
 @Configuration
 @RequiredArgsConstructor
 public class ElasticSearchConfiguration {
 
-    private final ElasticSearchProperties properties;
+  private final ElasticSearchProperties properties;
 
-    @Bean
-    public ElasticsearchClient elasticsearchClient() {
-        var creds = Base64.getEncoder()
-                .encodeToString((properties.getUsername() + ":" + properties.getPassword()).getBytes());
-        Rest5ClientBuilder restClient = Rest5Client
-                .builder(new HttpHost(properties.getHostname(), properties.getPort()))
-                .setDefaultHeaders(new Header[]{
-                        new BasicHeader("Authorization", "Basic " + creds)
-                });
-        ElasticsearchTransport transport = new Rest5ClientTransport(restClient.build(),
-                new JacksonJsonpMapper());
-        return new ElasticsearchClient(transport);
-    }
+  @Bean
+  public ElasticsearchClient elasticsearchClient() {
+    var creds = Base64.getEncoder()
+        .encodeToString((properties.getUsername() + ":" + properties.getPassword()).getBytes());
+    Rest5ClientBuilder restClient = Rest5Client
+        .builder(new HttpHost(properties.getHostname(), properties.getPort()))
+        .setDefaultHeaders(new Header[]{
+            new BasicHeader("Authorization", "Basic " + creds)
+        });
+    ElasticsearchTransport transport = new Rest5ClientTransport(restClient.build(),
+        new JacksonJsonpMapper());
+    return new ElasticsearchClient(transport);
+  }
 
 }
