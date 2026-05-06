76183a8fa27a24a07243509828ee96cfed240e11
southeo <soulaine.theo@gmail.com>
2026-05-06 13:48:03 +0200

    mas request batching should be false if not present (#204)
    * user errors become warnings
    
    * trivy
    
    * trivy

--- src/main/java/eu/dissco/backend/domain/MasJobRequest.java
+++ src/main/java/eu/dissco/backend/domain/MasJobRequest.java
@@ -2,8 +2,15 @@ package eu.dissco.backend.domain;
 
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Schema;
+import java.util.Objects;
 
 @Schema
 public record MasJobRequest(@Parameter(description = "ID of the MAS") String masId,
-		@Parameter(description = "If MAS should be run as a batch") boolean batching) {
+		@Parameter(description = "If MAS should be run as a batch") Boolean batching) {
+
+	public MasJobRequest(String masId, Boolean batching) {
+		this.masId = masId;
+		this.batching = Objects.requireNonNullElse(batching, Boolean.FALSE);
+	}
+
 }
