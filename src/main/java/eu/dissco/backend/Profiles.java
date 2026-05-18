8bc4ade026dee22cd0085833746189388c586cdc
Soulaine <soulaine.theo@gmail.com>
2026-05-18 16:22:33 +0200

    Media derivative behind feature toggle

---
+++ src/main/java/eu/dissco/backend/Profiles.java
@@ -0,0 +1,12 @@
+package eu.dissco.backend;
+
+public class Profiles {
+
+	public static final String DEFAULT = "default";
+
+	public static final String MEDIA_DERIVATIVES = "media-derivatives";
+
+	private Profiles() {
+	}
+
+}
--- src/main/java/eu/dissco/backend/properties/S3Properties.java
+++ src/main/java/eu/dissco/backend/properties/S3Properties.java
@@ -1,13 +1,16 @@
 package eu.dissco.backend.properties;
 
+import eu.dissco.backend.Profiles;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Profile;
 import org.springframework.validation.annotation.Validated;
 
 @Data
 @Validated
 @ConfigurationProperties(prefix = "s3")
+@Profile(Profiles.MEDIA_DERIVATIVES)
 public class S3Properties {
 
 	@NotBlank
