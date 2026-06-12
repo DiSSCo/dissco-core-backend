49e53cc77689860304759cc501fb84348aee4489
Sam Leeflang <sam.leeflang@naturalis.nl>
2026-06-12 11:44:12 +0200

    Fix update and tombstone VC handle request

--- src/main/java/eu/dissco/backend/utils/ProxyUtils.java
+++ src/main/java/eu/dissco/backend/utils/ProxyUtils.java
@@ -1,5 +1,7 @@
 package eu.dissco.backend.utils;
 
+import com.nimbusds.jose.util.Pair;
+
 public class ProxyUtils {
 
 	public static final String HANDLE_PROXY = "https://hdl.handle.net/";
@@ -12,6 +14,15 @@ public class ProxyUtils {
 
 	public static String removeHandleProxy(String id) {
 		return id.replace(HANDLE_PROXY, "");
+	}
+
+	public static Pair<String, String> splitHandle(String handle){
+		var splitHandle = handle.split("/");
+		if (splitHandle.length == 2){
+			return Pair.of(splitHandle[0], splitHandle[1]);
+		} else {
+		 throw new IllegalArgumentException("Invalid handle format: " + handle);
+		}
 	}
 
 	public static String removeDoiProxy(String id) {
